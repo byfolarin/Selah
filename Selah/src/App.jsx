58198adf51e67bdd958383fb3c652a94025507ea
Folarin Folarin <thefolafolarin@gmail.com>
2024-05-21 10:57:04 +0100

    initialization of h2

--- Selah/src/App.jsx
+++ Selah/src/App.jsx
@@ -1,9 +1,6 @@
-
-import './App.css'
 import Website from './components/website/Website'
 
 function App() {
-
 
   return (
     <>
--- Selah/src/components/website/Website.jsx
+++ Selah/src/components/website/Website.jsx
@@ -4,6 +4,8 @@ const Website = () => {
   return (
     <div className='wrapper'>
       <h1>Connect, Manage, Thrive: The <br /> Comprehensive Church Platform <br /> for Modern Ministry</h1>
+
+      <h2>With the endless possibilities in our software, you no longer have to worry about effectively managing your Church and administrative activities.</h2>
     </div>
   )
 }
