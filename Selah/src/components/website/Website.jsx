3093671ff833098507c9ef85b5538f9785f83d33
Folarin Folarin <thefolafolarin@gmail.com>
2024-05-21 12:15:46 +0100

    updated navigation

--- Selah/src/components/website/Website.jsx
+++ Selah/src/components/website/Website.jsx
@@ -3,6 +3,22 @@ import React from 'react'
 const Website = () => {
   return (
     <div className='wrapper'>
+
+     <div className="top-bar">
+
+      <div className="logo">
+        Selah!
+      </div>
+
+      <nav className='navigation'>
+          <ul>
+            <li>Product</li>
+            <li>About</li>
+          </ul>
+        </nav>
+
+        </div>
+
       <h1>Connect, Manage, Thrive: The <br /> Comprehensive Church Platform <br /> for Modern Ministry</h1>
 
       <h2>With the endless possibilities in our software, you no longer have to worry about effectively managing your Church and administrative activities.</h2>
