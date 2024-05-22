0e2eb221eb9ffa542e5ba35e8be3d9824a3f9076
Folarin Folarin <thefolafolarin@gmail.com>
2024-05-22 13:59:16 +0100

    updates,landing section-2

--- Selah/src/components/website/Website.jsx
+++ Selah/src/components/website/Website.jsx
@@ -2,32 +2,134 @@ import React from 'react'
 
 const Website = () => {
   return (
+
+
     <div className='wrapper'>
       <div className="container">
+
         <div className="top-bar">
+             <div className="logo">
+               Selah!
+              </div>
 
-      <div className="logo">
-        Selah!
-      </div>
-
-      <nav className='navigation'>
-          <ul>
-            <li>Case</li>
-            <li>About</li>
-          </ul>
-        </nav>
-
+            <nav className='navigation'>
+               <ul>
+                 <li>Case</li>
+                 <li>About</li>
+              </ul>
+           </nav>
         </div>
 
-      <h1>Connect, Manage, Thrive: The <br /> Comprehensive Church Platform <br /> for Modern Ministry</h1>
 
-      <h2>With the endless possibilities in our software, you no longer have to worry about effectively managing your Church and administrative activities.</h2>
+
+        <h1>Connect, Manage, Thrive: The <br /> 
+            Comprehensive Church Platform <br /> 
+            for Modern Ministry
+        </h1>
+
+        <h2>With the endless possibilities in our software, 
+            you no longer have to worry about effectively managing
+            your Church and administrative activities.
+        </h2>
 
       <div className="get-started-button">
-        <button className='Sign-up-button'>Register to our Mailing List!</button>
+        <button className='Sign-up-button'>
+          Register to our Mailing List!
+          </button>
+      </div>
+
+
+
+      <div className="section-2">
+
+      <div className='secondary-heading'>
+      <h1 >
+        What are we Building
+      </h1>
+
+      <h2>
+          Everything you need to administer your Church.<br /> 
+        While you handle offline, we help you set up online
+      </h2>
+      </div>
+
+
+
+      <div className="features-list">
+            <div className="list-style-type">
+
+          <div>
+          <h3>
+            1. Live Services
+          </h3>
+
+          <p className='this-class'>
+          Effortlessly plan and create events while seamlessly tracking attendance. 
+          </p>
+          </div>
+
+          <div>
+          <h3>
+            2. Member Management
+          </h3>
+
+          <p className='this-class'>
+          Effortlessly plan and create events while seamlessly tracking attendance. 
+          </p>
+          </div>
+
+          <div>
+          <h3>
+            3. Group Messaging
+          </h3>
+
+          <p className='this-class'>
+          Effortlessly plan and create events while seamlessly tracking attendance. 
+          </p>
+          </div>
+
+            </div>
+
+             <div className="list-style-type">
+
+          <div>
+          <h3>
+            4. Donation Integration
+          </h3>
+
+          <p className='this-class'>
+          Effortlessly plan and create events while seamlessly tracking attendance. 
+          </p>
+          </div>
+
+          <div>
+          <h3>
+            5. Community Management
+          </h3>
+
+          <p className='this-class'>
+          Effortlessly plan and create events while seamlessly tracking attendance. 
+          </p>
+          </div>
+
+          <div>
+          <h3>
+           6. Payroll System
+          </h3>
+
+          <p className='this-class'>
+          Effortlessly plan and create events while seamlessly tracking attendance. 
+          </p>
+          </div>
+
+            </div>
       </div>
 
       </div>
+
+      </div>
+
+
     </div>
   )
 }
