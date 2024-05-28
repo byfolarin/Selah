e6fd87f2acbef6c8e14111ec03726428f536c508
Folarin Folarin <thefolafolarin@gmail.com>
2024-05-28 08:40:30 +0100

    understanding layout

--- Selah/src/components/website/About.jsx
+++ Selah/src/components/website/About.jsx
@@ -10,6 +10,10 @@ const About = () => {
         and adapted into the digital age. 
         </h2>
 
+        <h2> The Church has an organized process, that needs to be documented <br />
+        and adapted into the digital age. 
+        </h2>
+
       <div className="get-started-button">
         <button className='Sign-up-button'>
           Register to our Mailing List
