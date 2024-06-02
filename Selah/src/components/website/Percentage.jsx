9a5d72ff267f9a8493b4757d76d3dabbaa6c3226
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-03 00:43:37 +0100

    understand react

--- Selah/src/components/website/Percentage.jsx
+++ Selah/src/components/website/Percentage.jsx
@@ -6,15 +6,37 @@ const Percentage = () => {
             const [tipes, setTipes] = useState("")
 
 
+            function clickBait (e) {
+                setTypes(e.target.value)
+            }
+
+            function clickHandle (e) {
+                setTipes(e.target.value)
+            }
+
+          
+
+            function clickSort(){
+                const calculation = Math.round(Math.random() * 100);
+                const stingy = `${clickHandle()} and ${clickBait} are ${calculation} compatible`
+
+                return stingy
+            }
+
+
   return (
     <div className='contains'>
 
     <div className="inputss">
-      <input type="text" value={types} placeholder='Your name' />
-      <input type="text" value={tipes} placeholder='Your partners Name'/>
+      <input type="text" value={types} onChange={clickBait} placeholder='Your name' />
+      <input type="text"  value={tipes} onChange={clickHandle} placeholder='Your partners Name'/>
     </div>
 
-    <button className='submit-btn'>Submit</button>
+    <h5>
+       {clickSort()}
+    </h5>
+
+    <button className='submit-btn' onClick={clickSort}>Submit</button>
     </div>
   )
 }
