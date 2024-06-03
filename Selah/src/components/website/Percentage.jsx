80f0576de75cac1824a37c7e0086a3da14ed8fa2
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-03 01:54:44 +0100

    didn't use state management for message

--- Selah/src/components/website/Percentage.jsx
+++ Selah/src/components/website/Percentage.jsx
@@ -1,42 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 const Percentage = () => {
+    const [name, setName] = useState("");
+    const [partner, setPartner] = useState("");
+    const [message, setMessage] = useState("");
 
-            const [name,setName] = useState("")
-            const [partner, setPartner] = useState("")
+    function clickBait(e) {
+        setName(e.target.value);
+    }
 
+    function clickHandle(e) {
+        setPartner(e.target.value);
+    }
 
-            function clickBait (e) {
-                setName(e.target.value)
-            }
+    useEffect
 
-            function clickHandle (e) {
-                setPartner(e.target.value)
-            }
+    function clickSort() {
+        const calculation = Math.round(Math.random() * 100);
+        const newMessage = `${name} and ${partner} are ${calculation}% compatible`;
+        setMessage(newMessage); // Update state with the new message
+    }
 
-          
+    return (
+        <div className='contains'>
+            <div className="inputss">
+                <input type="text" value={name} onChange={clickBait} placeholder='Your name' />
+                <input type="text" value={partner} onChange={clickHandle} placeholder='Your partners name'/>
+            </div>
 
-            function clickSort(){
-                const calculation = Math.round(Math.random() * 100);
-                return `${clickHandle} and ${clickBait} are ${calculation} compatible`
-            }
+            <h4>
+              {message}
+            </h4>
 
+            <button className='submit-btn' onClick={clickSort}>Submit</button>
+        </div>
+    );
+};
 
-  return (
-    <div className='contains'>
-
-    <div className="inputss">
-      <input type="text" value={name} onChange={clickBait} placeholder='Your name' />
-      <input type="text"  value={partner} onChange={clickHandle} placeholder='Your partners Name'/>
-    </div>
-
-    <h5>
-       {clickSort}
-    </h5>
-
-    <button className='submit-btn' onClick={clickSort}>Submit</button>
-    </div>
-  )
-}
-
-export default Percentage
+export default Percentage;
