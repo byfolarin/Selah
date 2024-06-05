4841edbe694e656d335cf27e8842003f1f6e9612
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-05 13:44:08 +0100

    calculator init

--- Selah/src/components/website/Calculator.jsx
+++ Selah/src/components/website/Calculator.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Calculator = () => {
   return (
-    <div>
-      
+    <div className='container'>
+      <p>This will change</p>
+
+      <div className="input">
+        <input type="text" />
+      </div>
+
+      <div className="new-btn">
+        <button>add</button>
+        <button>subtract</button>
+        <button>multiply</button>
+        <button>divide</button>
+        <button>reset input</button>
+        <button>reset result</button>
+      </div>
     </div>
   )
 }
