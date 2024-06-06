6d7dadc1e990134f017a8ec8b16469eafe65a7a3
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-06 12:40:14 +0100

    guessing input init

--- Selah/src/components/website/Guessing.jsx
+++ Selah/src/components/website/Guessing.jsx
@@ -23,8 +23,15 @@ import React from 'react'
 
 const Guessing = () => {
   return (
-    <div>
-      
+    <div className='container'>
+      <h1>Number Guessing Game</h1>
+      <p>We have selected a random number between 1 and 100. See if you can guess it in 10 turns or fewer. We'll tell you if your guess was too high or too low.</p>
+
+    <div className="submit-input">
+      <p>Enter a guess:</p>
+      <input type="text" />
+      <button>submit</button>
+      </div>
     </div>
   )
 }
