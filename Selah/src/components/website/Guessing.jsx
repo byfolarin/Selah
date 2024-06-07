7c28139227c1f2162a6afe44d3becee0ea80888a
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-07 12:24:53 +0100

    initializing arrays

--- Selah/src/components/website/Guessing.jsx
+++ Selah/src/components/website/Guessing.jsx
@@ -2,24 +2,36 @@ import React, { useEffect, useState } from 'react'
 
 const Guessing = () => {
 
-    const [text,setText] = useState('');
+    const [text,setText] = useState(0);
     const [guess, setGuess] = useState([])
-    const randomNumber = Math.floor(Math.random() * 100);
+    // const [random, setRandom] = useState('');
 
+    function clickHandle(e) {
+        setText(e.target.value);
+    }
 
+    const getText = () =>{
+        setText.map(i =>{
+        setGuess(...i, text)
+        })
+    };
+
+    // const getRandom = Math.floor(Math.random() * 100);
+    // setRandom(getRandom);
 
   return (
     <div className='container'>
       <h1>Number Guessing Game</h1>
-      <p>We have selected a random number between 1 and 100. See if you can guess it in 10 turns or fewer. We'll tell you if your guess was too high or too low.</p>
+      <h2>We have selected a random number between 1 and 100.
+        <br /> See if you can guess it in 10 turns or fewer. We'll tell you if your guess was too high or too low.</h2>
 
     <div className="submit-input">
-      <p>Enter a guess:{text}</p>
-      <input type="number"  name="Please fill out this" id="" />
-      <button>submit</button>
+      <p>Enter a guess:</p>
+      <input type="number" value={text} onChange={clickHandle} id="" />
+      <button onClick={getText} >submit</button>
       </div>
 
-      <p>Previous Guess:{guess}</p>
+      <p>Previous Guess:{getText}</p>
       <p>Wrong</p>
       <p>Last Guess was too Low</p>
     </div>
