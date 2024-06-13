8a8ad449dcd903f1b2b62eec5a0d33d9d4053473
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-13 12:40:49 +0100

    handleClick

--- Selah/src/components/website/Object.jsx
+++ Selah/src/components/website/Object.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Object = () => {
+
+    const [person, setPerson] = useState({greet: "Hey Good morning"});
+
+    const handleClick = () => {
+        const newPerson = {...person, meeting: "Hello there",};
+        setPerson(newPerson); 
+    };
+
+
   return (
     <div>
-      
+      <h1>{person.greet}</h1>
+      <button onClick={handleClick}>Change Greet</button>
     </div>
   )
 }
