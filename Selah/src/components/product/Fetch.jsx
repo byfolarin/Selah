2194c29e5aa7c33277b3f57b38474833a00efb57
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-18 09:09:25 +0100

    api call

--- Selah/src/components/product/Fetch.jsx
+++ Selah/src/components/product/Fetch.jsx
@@ -1,34 +1,29 @@
 import React from 'react'
+import { useState, useEffect } from "react"; 
 
 const Fetch = () => {
-
-    const url = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list'; 
-
-    fetch(url)
-
-      .then(response => {
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`); // Throw an error for non-200 status codes
-          }
-          return response.json(); // Parse the response as JSON
-        })
-
-      .then(data => {
-        console.log('Fetched data:', data); // Do something with the fetched data
-      })
-      
-      .catch(error => {
-        console.error('Error fetching data:', error); // Handle errors
-      });
-
-
-
-  return (
-    <div>
-      
-    </div>
-  )
+    
+    const [btcData, setBtcData] = useState({}); 
+        
+      useEffect(() => { 
+        fetch(`https://api.coindesk.com/v1/bpi/currentprice.json`) 
+          .then((response) => response.json()) 
+          .then((jsonData) => setBtcData(jsonData.bpi.USD)) 
+          .catch((error) => console.log(error)); 
+      }, []); 
+    
+    
+      return ( 
+        <> 
+          <h1>Current BTC/USD data</h1> 
+          <p>Code: {btcData.code}</p> 
+          <p>Symbol: {btcData.symbol}</p> 
+          <p>Rate: {btcData.rate}</p> 
+          <p>Description: {btcData.description}</p> 
+          <p>Rate Float: {btcData.rate_float}</p> 
+        </> 
+      ); 
+    
 }
 
 export default Fetch
